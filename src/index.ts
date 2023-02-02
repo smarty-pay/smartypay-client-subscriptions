83d8e344c4f88ee2c95c91c32ea36f5b83052953
edolganov <evgenij.dolganov@gmail.com>
2023-02-02 22:44:35 +0300

    Temp impl

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,16 @@
 import {
   clearLastWeb3ApiName,
   getLastWeb3ApiName,
-  storeLastWeb3ApiName,
+  storeLastWeb3ApiName, TokenMaxAbsoluteAmount,
   Web3Api,
   Web3ApiEvent,
-  Web3ApiProvider
+  Web3ApiProvider,
+  Web3Common
 } from 'smartypay-client-web3-common';
 import {
+  Assets,
+  CurrencyKeys,
+  Subscription,
   util,
 } from 'smartypay-client-model';
 
@@ -149,6 +153,58 @@ class SmartyPaySubscriptionsBrowserImpl {
         console.warn(`${Name}: Can not correctly disconnect the wallet ${wallet.name()}`, e);
       }
     });
+  }
+
+  async activateSubscriptionInWallet(subscriptionGetter: ()=>Promise<Subscription>){
+    await this.useApiLock('activateSubscription', async ()=>{
+
+      const wallet = this.getActiveWallet();
+      const address = await wallet.getAddress();
+      const subscription = await subscriptionGetter();
+
+      if(!subscription){
+        throw util.makeError('undefined subscription to activate');
+      }
+
+      const {
+        status,
+        amount: amountVal,
+        contractAddress
+      } = subscription;
+
+      // subscription already activated
+      if(status !== 'Draft'){
+        return;
+      }
+
+      const [amount, asset] = amountVal.split(' ');
+      const currency = CurrencyKeys.find(c => c === asset);
+      if( ! currency || currency === 'UNKNOWN'){
+        throw util.makeError('can not activate subscription: unknown amount currency', amountVal);
+      }
+
+      const token = Assets[currency];
+      const amountToPay = Web3Common.toAbsoluteForm(amount, token);
+
+      const curBalanceVal = await Web3Common.getTokenBalance(token, address);
+      const curBalance = Web3Common.toAbsoluteForm(curBalanceVal, token);
+      if(amountToPay.gt(curBalance)){
+        throw util.makeError(`Not enough ${token.abbr} token funds to activate the subscription`);
+      }
+
+      const curAllowanceVal = await Web3Common.getTokenAllowance(token, address, contractAddress);
+      const curAllowance = Web3Common.toAbsoluteForm(curAllowanceVal, token);
+
+      // take approval from wallet to spend a token by subscription contract
+      if(amountToPay.gt(curAllowance)){
+
+        const approveAmount = TokenMaxAbsoluteAmount;
+        const resultTx = await Web3Common.walletTokenApprove(wallet, token, address, contractAddress, approveAmount);
+
+        console.log('!! result tx', resultTx);
+      }
+
+    })
   }
 
   isApiLocked(){
