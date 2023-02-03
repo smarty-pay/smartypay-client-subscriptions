35e2804d85a287e7bac9415f1804a9a573b1b893
edolganov <evgenij.dolganov@gmail.com>
2023-02-03 14:18:19 +0300

    Temp impl

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@
   @author Evgeny Dolganov <evgenij.dolganov@gmail.com>
 */
 import {TokenMaxAbsoluteAmount, wallet, Web3ApiProvider, Web3Common,} from 'smartypay-client-web3-common';
-import {Assets, CurrencyKeys, Subscription, util,} from 'smartypay-client-model';
+import {Assets, CurrencyKeys, Subscription, SubscriptionStatus, util,} from 'smartypay-client-model';
 import {findApiByContactAddress} from './util';
+import {getJsonFetcher, postJsonFetcher} from './util/fetch-util';
 
 
-export type SmartyPaySubscriptionsBrowserEvent = wallet.WalletApiEvent;
+export type SmartyPaySubscriptionsBrowserEvent =
+  wallet.WalletApiEvent
+  | 'subscription-updating';
 
 
 export interface SmartyPaySubscriptionsBrowserProp {
@@ -18,6 +21,7 @@ export interface SmartyPaySubscriptionsBrowserProp {
 class SmartyPaySubscriptionsBrowserImpl extends wallet.WalletApi<SmartyPaySubscriptionsBrowserEvent> {
 
   private props: SmartyPaySubscriptionsBrowserProp|undefined;
+  private updatingSubscriptions = new Set<string>();
 
   constructor(props?: SmartyPaySubscriptionsBrowserProp) {
     super('SmartyPaySubscriptionsBrowser');
@@ -32,6 +36,10 @@ class SmartyPaySubscriptionsBrowserImpl extends wallet.WalletApi<SmartyPaySubscr
     super.addListener(event as any, listener);
   }
 
+  getUpdatingSubscriptions(): string[]{
+    return Array.from(this.updatingSubscriptions);
+  }
+
   async activateSubscriptionInWallet(subscriptionGetter: ()=>Promise<Subscription>){
     await this.useApiLock('activateSubscription', async ()=>{
 
@@ -40,7 +48,7 @@ class SmartyPaySubscriptionsBrowserImpl extends wallet.WalletApi<SmartyPaySubscr
       const subscription = await subscriptionGetter();
 
       if(!subscription){
-        throw util.makeError('undefined subscription to activate');
+        throw util.makeError('Undefined subscription to activate');
       }
 
       const {
@@ -57,7 +65,7 @@ class SmartyPaySubscriptionsBrowserImpl extends wallet.WalletApi<SmartyPaySubscr
       const [amount, asset] = amountVal.split(' ');
       const currency = CurrencyKeys.find(c => c === asset);
       if( ! currency || currency === 'UNKNOWN'){
-        throw util.makeError('can not activate subscription: unknown amount currency', amountVal);
+        throw util.makeError('Can not activate subscription: unknown amount currency', amountVal);
       }
 
       const token = Assets[currency];
@@ -69,34 +77,75 @@ class SmartyPaySubscriptionsBrowserImpl extends wallet.WalletApi<SmartyPaySubscr
         throw util.makeError(`Not enough ${token.abbr} token funds to activate the subscription`);
       }
 
-      const curAllowanceVal = await Web3Common.getTokenAllowance(token, address, contractAddress);
-      const curAllowance = Web3Common.toAbsoluteForm(curAllowanceVal, token);
-
       // take approval from wallet to spend a token by subscription contract
-      if(amountToPay.gt(curAllowance)){
-
-        try {
-          const resultTx = await Web3Common.walletTokenApprove(
-            wallet,
-            token,
-            address,
-            contractAddress,
-            TokenMaxAbsoluteAmount
-          );
-
-          this.getListeners().fireEvent('blockchain-transaction', 'token-approve-tx', resultTx);
-        } catch (e){
-          // skip long error info
-          throw new Error('Can not approve token amount.');
-        }
+      let approveTx: string;
+      try {
+        approveTx = await Web3Common.walletTokenApprove(
+          wallet,
+          token,
+          address,
+          contractAddress,
+          TokenMaxAbsoluteAmount
+        );
+        this.fireEvent('blockchain-transaction', 'token-approve-tx', approveTx);
+      } catch (e){
+        // skip long error info
+        throw util.makeError(this.name, 'Can not approve token amount.');
       }
 
-      const apiUrl = await findApiByContactAddress(contractAddress);
+      // direct api notification
+      const apiUrl = await this.getCheckStatusUrl(contractAddress);
+      const {isAccepted} = await postJsonFetcher(`${apiUrl}/integration/subscriptions/hint-update-state`, {
+        hash: approveTx,
+        blockchain: token.network
+      });
 
-      // todo call hint activation
-      console.log('!! activateSubscriptionInWallet success done', apiUrl)
+      if( ! isAccepted){
+        throw util.makeError(this.name, 'Can not notify SmartyPay server about subscription');
+      }
 
+      // async check subscription status update
+      this.waitSubscriptionStatusUpdate(contractAddress, status, 'Active').catch(console.error);
     })
+  }
+
+  private async waitSubscriptionStatusUpdate(
+    contractAddress: string,
+    initStatus: SubscriptionStatus,
+    targetStatus?: SubscriptionStatus){
+
+    // already in set
+    if(this.updatingSubscriptions.has(contractAddress)){
+      return;
+    }
+
+    const apiUrl = await this.getCheckStatusUrl(contractAddress);
+    const waitNextTryDelta = 8000;
+    const stopWaitTimeout = Date.now() + waitNextTryDelta * 5;
+
+    this.updatingSubscriptions.add(contractAddress);
+    this.fireEvent('subscription-updating', contractAddress, true);
+
+    const onDone = ()=>{
+      this.updatingSubscriptions.delete(contractAddress);
+      this.fireEvent('subscription-updating', contractAddress, false);
+    }
+
+    while(Date.now() <= stopWaitTimeout){
+
+      // wait delta
+      await util.waitTimeout(waitNextTryDelta);
+
+      // check status
+      const {status} = await getJsonFetcher(`${apiUrl}/integration/subscriptions/${contractAddress}/status`);
+      if(status !== initStatus && (!targetStatus || targetStatus === status)){
+        onDone();
+        return;
+      }
+    }
+
+    // timeout
+    onDone();
   }
 
   private async getCheckStatusUrl(contractAddress: string): Promise<string>{
